f0fcdd79c6c0d10c4d2dcac2bdd9934e772b19f0
Francis Berger <frcs.berger@gmail.com>
2024-11-14 19:30:41 +0100

    feat: setup

--- src/components/layouts/DefaultFallback.tsx
+++ src/components/layouts/DefaultFallback.tsx
@@ -6,12 +6,12 @@ export default function DefaultFallback() {
             <PageWrapper className="gap-5">
                 <div className="flex w-full flex-col items-center justify-center gap-3">
                     <div className="h-6 w-full max-w-52 animate-pulse rounded-md bg-light-hover" />
-                    <div className="relative flex h-[420px] w-full animate-pulse flex-col gap-1 border border-inactive bg-light-hover md:h-[calc(100vh-280px)]">
-                        <div className="h-11 w-full rounded-sm border-b border-inactive" />
-                        {Array(20)
+                    <div className="relative flex h-[420px] w-full animate-pulse flex-col gap-1 border border-inactive bg-background md:h-[calc(100vh-280px)]">
+                        <div className="h-11 w-full rounded-sm border-b border-inactive bg-light-hover" />
+                        {Array(24)
                             .fill('-')
                             .map((entry, entryIndex) => (
-                                <div key={`${entry}-${entryIndex}`} className="h-6 w-full rounded-sm bg-very-light-hover p-2" />
+                                <div key={`${entry}-${entryIndex}`} className="h-6 w-full rounded-sm bg-very-light-hover opacity-50" />
                             ))}
                         <p className="absolute bottom-1/2 w-full text-center text-primary">Loading App ...</p>
                     </div>
