d6a350eeae6276cce9067ae84ffc2c2d195c8fae
Francis Berger <frcs.berger@gmail.com>
2024-11-20 11:21:35 +0100

    feat: setup

--- src/hooks/useKeyboardShortcutArgs.ts
+++ src/hooks/useKeyboardShortcutArgs.ts
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 
+// https://dev.to/brdnicolas/click-outside-magic-a-new-custom-hook-4np4
 export function useKeyboardShortcut({ key, onKeyPressed }: { key: string; onKeyPressed: () => void }) {
     useEffect(() => {
         function keyDownHandler(e: globalThis.KeyboardEvent) {
